c911ebd92c9abfecedcf9ede3fb3fba6b7871387
Said1224nr <162983493+Said1224nr@users.noreply.github.com>
2024-05-16 19:43:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let sonPrompt = +prompt("Son kiriting !")
+
+switch (sonPrompt) {
+    case 1: console.log("dushanba"); break
+    case 2: console.log("seshanba"); break
+    case 3: console.log("chorshanba"); break
+    case 4: console.log("payshanba"); break
+    case 5: console.log("juma"); break
+    case 6: console.log("shanba"); break
+    case 7: console.log("yakshanba"); break
+
+    default: console.log("Bunday kun yo'q !");
+}
